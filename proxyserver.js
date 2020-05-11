823096e3bb606987a8b69b331078e0aa73acf7e3
practiceland <the.teepee.creeper@gmail.com>
2020-05-11 12:30:18 -0700

    Defining tentative routes for each service

--- proxyserver.js
+++ proxyserver.js
@@ -1,3 +1,40 @@
 var express = require('express'); 
 var request = require('request'); 
 var app = express();
+
+app.get('/travelers*', function(req, res) {
+  /* request( url and endpoint, function (error, response, body) {
+    if( !error && response.statusCode === 200 ) {
+      res.send(body)
+    }
+  })*/
+}); 
+
+app.get('/reviews*', function(req, res) {
+  /* request( url and endpoint, function (error, response, body) {
+    if( !error && response.statusCode === 200 ) {
+      res.send(body)
+    }
+  })*/
+});
+
+app.get('/gallery*', function(req, res) {
+  /* request( url and endpoint, function (error, response, body) {
+    if( !error && response.statusCode === 200 ) {
+      res.send(body)
+    }
+  })*/
+});
+
+app.get('/itinerary*', function(req, res) {
+  /* request( url and endpoint, function (error, response, body) {
+    if( !error && response.statusCode === 200 ) {
+      res.send(body)
+    }
+  })*/
+});
+
+const proxyport = 5000;
+
+app.listen(proxyport);
+console.log( `Server running on port ${proxyport}`);
