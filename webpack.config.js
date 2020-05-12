3365dec4c0515ce63df6fcaa08d37062b7799383
practiceland <the.teepee.creeper@gmail.com>
2020-05-12 12:18:46 -0700

    Webpack setup for react

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
-const path = require ('path');
-
+const path = require('path');
 module.exports = {
-  entry: path.resolve(__dirname, "src", "main.js"),
+  entry: path.resolve(__dirname, 'src', 'main.js'),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  }
-  mode: "development",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 }
