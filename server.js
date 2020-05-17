f1df10494bd7be456f0e400dfa4742ee0d8cf0a1
practiceland <the.teepee.creeper@gmail.com>
2020-05-17 16:49:38 -0700

    Calendar works

--- server.js
+++ server.js
@@ -3,31 +3,41 @@ const request = require('request');
 const path = require('path');
 const app = express();
 
+
+const proxyport = process.env.PORT || 5000;
+const itineraryserver = process.env.ITINERARY || 'http://54.190.52.239';
+const calendarserver = process.env.CALENDAR || 'http://3.23.167.116';
+const reviewserver = process.env.REVIEWS || 'localhost:3001';
+const galleryserver = process.env.GALLERY || 'http://13.52.101.132';
+
 app.use(express.static(path.join(__dirname)));
 
 app.get('', (req, res) => {
   res.sendFile("index.html");
 })
 
-app.get('/:id/price', function(req, res) {
-  request( 'http://localhost:3002/api/trip/:id/price', function (error, response, body) {
+app.get('/api/trip/:id/price', function(req, res) {
+    console.log("Calendar get");
+
+  request( `${calendarserver}/api/trip/${req.params.id}/price`, function (error, response, body) {
     if( !error && response.statusCode === 200 ) {
       res.send(body)
     }
   })
 }); 
 
-app.get('/:id/calendar', function(req, res) {
-  request( 'http://localhost:3002/api/trip/:id/calendar', function (error, response, body) {
+app.get('/api/trip/:id/calendar', function(req, res) {
+  console.log("Calendar get");
+  request( `${calendarserver}/${req.params.id}/calendar`, function (error, response, body) {
     if( !error && response.statusCode === 200 ) {
+      console.log(response.json());
       res.send(body)
     }
   })
 }); 
 
 app.get('/reviews/:id', function(req, res) {
-  console.log("camerooooooon");
-  request( 'http://localhost:3001/reviews/:id', function (error, response, body) {
+  request( `${reviewserver}/reviews/${req.params.id}`, function (error, response, body) {
     if( !error && response.statusCode === 200 ) {
       res.send(body)
     }
@@ -35,51 +45,47 @@ app.get('/reviews/:id', function(req, res) {
 });
 
 app.get('/reviews/', function(req, res) {
-   request( 'http://localhost:3001/reviews/', function (error, response, body) {
+   request(`${reviewserver}/reviews`, function (error, response, body) {
     if( !error && response.statusCode === 200 ) {
       res.send(body)
     }
   })
 });
 
-app.put('/reviews/', function(req, res) {
-  request( 'http://localhost:3001/reviews/', function (error, response, body) {
+app.put('/reviews', ({ body: { _id } }, res) => {
+  request( `${galleryserver}/reviews`, function(error, response, body) {
     if( !error && response.statusCode === 200 ) {
       res.send(body)
     }
   })
 });
-
 
 app.get('/tripAdvisor/:id/gallery', function(req, res) {
-  request( 'http://localhost:9999/tripAdvisor/:id/gallery', function (error, response, body) {
+  console.log(req.params.id);
+  request( `${galleryserver}/tripAdvisor/${req.params.id}/gallery`, function (error, response, body) {
+    console.log("Response:")
+    console.log(response);
     if( !error && response.statusCode === 200 ) {
       res.send(body)
     }
   })
 });
 
-// app.get('/tour/:id', function(req, res) {
-//   /* request( url and endpoint, function (error, response, body) {
-//     if( !error && response.statusCode === 200 ) {
-//       res.send(body)
-//     }
-//   })*/
-// });
+app.get('/tour/:id', function(req, res) {
+  request( `${itineraryserver}/tour/${req.params.id}`, function (error, response, body) {
+    if( !error && response.statusCode === 200 ) {
+      res.send(body)
+    }
+  })
+});
 
 app.get('/tour', function(req, res) {
-   request( 'http://localhost:3000/tour', function (error, response, body) {
+   request( `${itineraryserver}/tour/`, function (error, response, body) {
     if( !error && response.statusCode === 200 ) {
       res.send(body)
     }
   })
 });
-
-
-// https://stackoverflow.com/questions/10435407/proxy-with-express-js
-// https://www.youtube.com/watch?v=1xo-0gCVhTU
-
-const proxyport = 5000;
 
 app.listen(proxyport);
 console.log( `Server running on port ${proxyport}`);
